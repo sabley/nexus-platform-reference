f6a415c5a9c39399bfc2f0a310a54c902d60845a
CMYanko <cmyanko@gmail.com>
2018-12-12 10:55:21 -0500

    removed IQ section and mkdir commands as they aren't needed

--- demo-setup.sh
+++ demo-setup.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# Creates directories to be mounted to containers as volumes
-mkdir ~/iq-data ~/nexus-data
-
 # Stands up test environment and builds nginix container to put our config in
 docker-compose up -d
 
 until curl --fail --insecure http://localhost:8070; do 
   sleep 5
 done
-
-#import license and policies to IQ server
-./iq-server/config-iq.sh
 
 #Create Docker repos and group
 cd nexus-repository
